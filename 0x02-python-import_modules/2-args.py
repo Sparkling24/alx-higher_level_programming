a9914f645fb2c210af6cd2790dc687505431a406
Sparkling24 <chimexdguy2020@gmail.com>
2024-03-17 04:19:40 -0700

    9298

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
 
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+
+    if argc > 0:
+        for i in range(argc):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == "__main__":
+    import sys
+    sum = 0
+
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            sum += int(arg)
+    print("{}".format(sum))
