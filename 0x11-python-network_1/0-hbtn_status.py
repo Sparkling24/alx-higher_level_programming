67307fa3eee73ece0482ef97d779a28c54bf5b2c
Sparkling24 <chimexdguy2020@gmail.com>
2024-07-05 14:13:08 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""This is a Python script that fetches https://alx-intranet.hbtn.io/status
-uses urllib pakage"""
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-if __name__ == "__main__":
-import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status/') as response:
-    content = res.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
